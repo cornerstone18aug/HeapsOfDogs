c026f4d2f33b01b1a7271b8a1a68b91e8de850ad
nator333 <m.nakamata35@gmail.com>
2018-11-15 10:31:11 -0800

    Implement VCQueue

--- src/main/java/PriorityQueue.java
+++ src/main/java/PriorityQueue.java
@@ -1,3 +1,36 @@
-public class PriorityQueue {
+/**
+ * PriorityQueue
+ * @author Masa, Hao-tse
+ */
+public class PriorityQueue implements VCPriorityQueue {
 
+  @Override
+  public int size() {
+    return 0;
+  }
+
+  @Override
+  public boolean isEmpty() {
+    return false;
+  }
+
+  @Override
+  public Entry enqueue(Comparable key, Object value) throws IllegalArgumentException {
+    return null;
+  }
+
+  @Override
+  public Entry peek() {
+    return null;
+  }
+
+  @Override
+  public Entry dequeueMin() {
+    return null;
+  }
+
+  @Override
+  public VCPriorityQueue merge(VCPriorityQueue other) {
+    return null;
+  }
 }
