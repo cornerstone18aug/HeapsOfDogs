83a06b9eb5476343dadc8869119cabc771e632fc
nator333 <m.nakamata35@gmail.com>
2018-11-18 09:36:41 -0800

    Implement BHQueue

--- src/main/java/BHPriorityQueue.java
+++ src/main/java/BHPriorityQueue.java
@@ -1,6 +1,7 @@
 import base.Entry;
 import base.VCPriorityQueue;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -10,7 +11,23 @@ import java.util.List;
  */
 public class BHPriorityQueue<K extends Comparable, V> implements VCPriorityQueue<K, V> {
 
-  private List<Entry<K, V>> heapList = new ArrayList<>();
+  List<Entry<K, V>> heapList = new ArrayList<>();
+
+  private enum CompResult {
+    EQUAL(0), BIGGER(1), SMALLER(-1);
+
+    CompResult(int num) {
+      this.num = num;
+    }
+
+    public int num;
+
+    public static CompResult find(int compareToNum) {
+      return Arrays.stream(values())
+          .filter(compResult -> compResult.num == compareToNum)
+          .findFirst().get();
+    }
+  }
 
   @Override
   public int size() {
@@ -27,43 +44,71 @@ public class BHPriorityQueue<K extends Comparable, V> implements VCPriorityQueue
     if (key == null) {
       throw new IllegalArgumentException();
     }
-    Entry<K, V> element = new Entry<>(key, value);
 
-    int newIndex = this.size();
+    // Create new Entry
+    Entry<K, V> newEntry = new Entry<>(key, value);
+    this.heapList.add(newEntry);
 
-    if (this.isEmpty()) {
-      heapList.add(element);
-    } else if (this.size() + 1 % 2 == 0) {
-      switch (heapList.get(this.size() - 1).getKey()
-          .compareTo(element.getKey())) {
-        case 0:
-        case 1:
-          heapList.add(this.size() - 1, element);
-          heapList.set
-          break;
-        case -1:
-          heapList.add(element);
-          break;
-      }
-    } else {
-      switch (heapList.get(this.size() - 1).getKey()
-          .compareTo(element.getKey())) {
-        case 0:
-        case 1:
-          heapList.add(this.size() - 1, element);
-          break;
-        case -1:
-          heapList.add(element);
-          break;
-      }
+    if (this.heapList.size() == 1) {
+      // First element
+      return newEntry;
     }
 
-    return element;
+    int currentIndex;
+    int parentIndex;
+    int adjacentIndex;
+    Entry<K, V> parentEntry;
+    Entry<K, V> adjacentEntry;
+
+    while (!isRoot(newEntry)) {
+      currentIndex = getCurrentIndex(newEntry);
+      parentIndex = getParentIndex(currentIndex);
+      parentEntry = heapList.get(parentIndex);
+
+      if (isLeft(newEntry)) {
+        // Leftmost element
+        switch (compare(parentEntry, newEntry)) {
+          case EQUAL:
+          case BIGGER:
+            // Swap with parent
+            swap(parentEntry, newEntry);
+
+            break;
+          case SMALLER:
+            return newEntry;
+        }
+      } else {
+        // Rightmost element
+        adjacentIndex = getAdjacentIndex(currentIndex);
+        adjacentEntry = heapList.get(adjacentIndex);
+        switch (compare(adjacentEntry, newEntry)) {
+          // Adjacent was same or smaller
+          case EQUAL:
+          case SMALLER:
+            return newEntry;
+          // Adjacent was bigger, compare to parent
+          case BIGGER:
+            switch (compare(parentEntry, newEntry)) {
+              case EQUAL:
+              case SMALLER:
+                return newEntry;
+              case BIGGER:
+                // Swap with parent
+                swap(parentEntry, newEntry);
+                break;
+            }
+            break;
+        }
+      }
+
+    }
+
+    return newEntry;
   }
 
   @Override
   public Entry<K, V> peek() {
-    if (this.heapList.size() == 0) {
+    if (this.isEmpty()) {
       return null;
     }
     return this.heapList.get(0);
@@ -71,15 +116,152 @@ public class BHPriorityQueue<K extends Comparable, V> implements VCPriorityQueue
 
   @Override
   public Entry<K, V> dequeueMin() {
-    if (this.heapList.size() == 0) {
+    if (this.isEmpty()) {
       return null;
     }
 
-    return null;
+    // Create new Entry
+    int lastIndex= heapList.size() - 1;
+    Entry<K, V> dequeueEntry = getEntry(0);
+    Entry<K, V> lastEntry = getEntry(lastIndex);
+    this.heapList.remove(lastEntry);
+    this.heapList.set(0 ,lastEntry);
+
+    int currentIndex;
+    int leftChildIndex;
+    int rightChildIndex;
+    Entry<K, V> leftChildEntry;
+    Entry<K, V> rightChildEntry;
+
+    int count = 0;
+    while (!isLast(lastEntry) || count == heapList.size()) {
+      currentIndex = getCurrentIndex(lastEntry);
+
+      leftChildIndex = getLeftChildIndex(currentIndex);
+      leftChildEntry = getEntry(leftChildIndex);
+      
+      rightChildIndex = getRightChildIndex(currentIndex);
+      rightChildEntry = getEntry(rightChildIndex);
+
+      // Either child was null
+      if (leftChildEntry == null || rightChildEntry == null) {
+        if (leftChildEntry != null) {
+          if (compare(leftChildEntry, lastEntry) == CompResult.SMALLER) {
+            swap(leftChildEntry, lastEntry);
+          }
+        } else if (rightChildEntry != null){
+          if (compare(rightChildEntry, lastEntry) == CompResult.SMALLER) {
+            swap(rightChildEntry, lastEntry);
+          }
+        }
+        return dequeueEntry;
+      }
+
+      // Completely sort between 3(self and 2 children)
+      switch (compare(leftChildEntry, rightChildEntry)) {
+        case BIGGER:
+        case EQUAL:
+          swap(lastEntry, rightChildEntry);
+          break;
+        case SMALLER:
+          swap(leftChildEntry, lastEntry);
+          break;
+      }
+
+      ++count;
+    }
+
+    return dequeueEntry;
   }
 
   @Override
-  public VCPriorityQueue<K, V> merge(VCPriorityQueue<K, V> other) {
-    return null;
+  public BHPriorityQueue<K, V> merge(VCPriorityQueue<K, V> other) {
+    while (!other.isEmpty()) {
+      Entry<K, V> entry = other.dequeueMin();
+      enqueue(entry.getKey(), entry.getValue());
+    }
+    return this;
+  }
+
+  private boolean isLeft(Entry<K, V> entry) {
+    return getCurrentIndex(entry) % 2 == 1;
+  }
+
+  private boolean isRight(Entry<K, V> entry) {
+    return getCurrentIndex(entry) % 2 == 0;
+  }
+
+  private boolean isRoot(Entry<K, V> entry) {
+    return getCurrentIndex(entry) == 0;
+  }
+
+  private boolean isLast(Entry<K, V> entry) {
+    return getCurrentIndex(entry) == this.heapList.size() - 1;
+  }
+
+  private int getCurrentIndex(Entry<K, V> entry) {
+    return this.heapList.indexOf(entry);
+  }
+
+  private int getParentIndex(Entry<K, V> entry) {
+    return getParentIndex(getCurrentIndex(entry));
+  }
+
+  private int getAdjacentIndex(Entry<K, V> entry) {
+    return getAdjacentIndex(getCurrentIndex(entry));
+  }
+
+  private int getLeftChildIndex(Entry<K, V> entry) {
+    return getLeftChildIndex(getCurrentIndex(entry));
+  }
+
+  private int getRightChildIndex(Entry<K, V> entry) {
+    return getRightChildIndex(getCurrentIndex(entry));
+  }
+
+
+
+  private int getParentIndex(int currentIndex) {
+    if (isLeft(this.heapList.get(currentIndex))) {
+      return (currentIndex - 1) / 2;
+    } else {
+      return (currentIndex / 2) - 1;
+    }
+  }
+
+  private int getAdjacentIndex(int currentIndex) {
+    if (isLeft(this.heapList.get(currentIndex))) {
+      return currentIndex + 1;
+    } else {
+      return currentIndex - 1;
+    }
+  }
+
+  private int getLeftChildIndex(int currentIndex) {
+    return (currentIndex * 2) + 1;
+  }
+
+  private int getRightChildIndex(int currentIndex) {
+    return (currentIndex + 1) * 2;
+  }
+
+  private CompResult compare(Entry<K, V> e1, Entry<K, V> e2) {
+    return CompResult.find(e1.getKey().compareTo(e2.getKey()));
+  }
+
+  private void swap(Entry<K, V> e1, Entry<K, V> e2) {
+    int swapIndex = getCurrentIndex(e2);
+    this.heapList.set(getCurrentIndex(e1), e2);
+    this.heapList.set(swapIndex, e1);
+  }
+
+  private Entry<K, V> getEntry(int index){
+    Entry<K, V> result = null;
+
+    if (this.heapList.size() > index) {
+      result = this.heapList.get(index);
+    }
+
+    return result;
   }
 }
--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,5 +1,47 @@
+import base.Entry;
+
 public class Driver {
   public static void main(String[] args) {
-    ALPriorityQueue<String, String> queue = new ALPriorityQueue<>();
+    BHPriorityQueue<Integer, Integer> bhQueue = new BHPriorityQueue<>();
+
+    bhQueue.enqueue(1, 1);
+    bhQueue.enqueue(2, 2);
+    bhQueue.enqueue(4, 4);
+    bhQueue.enqueue(3, 3);
+    bhQueue.enqueue(2, 2);
+    bhQueue.enqueue(3, 3);
+
+    for (Entry<Integer, Integer> entry: bhQueue.heapList){
+      System.out.println(entry.getKey());
+    }
+
+    System.out.println("===============");
+
+    bhQueue.dequeueMin();
+
+    for (Entry<Integer, Integer> entry: bhQueue.heapList){
+      System.out.println(entry.getKey());
+    }
+
+    System.out.println("===============");
+
+    bhQueue.dequeueMin();
+
+    for (Entry<Integer, Integer> entry: bhQueue.heapList){
+      System.out.println(entry.getKey());
+    }
+
+    System.out.println("===============");
+
+    BHPriorityQueue<Integer, Integer> margeQueue = new BHPriorityQueue<>();
+
+    bhQueue.enqueue(5, 1);
+    bhQueue.enqueue(6, 2);
+    bhQueue.enqueue(1, 4);
+
+    for (Entry<Integer, Integer> entry: bhQueue.merge(margeQueue).heapList){
+      System.out.println(entry.getKey());
+    }
+
   }
 }
