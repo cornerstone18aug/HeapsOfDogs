cd63cbdc6e1a79555b8cc0b16a51be8bc29d4812
Hao-Tse <maxwuhz@gmail.com>
2018-11-19 13:14:43 -0800

    Revert "Upload BHPqueue"
    This reverts commit 420933858eb64c47605d60586cc73ac24ab0a217.

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -1,40 +1,13 @@
-
-import java.util.ArrayList;
-
 public class Driver {
   public static void main(String[] args) {
-//    ALPriorityQueue<Integer,Integer> alque = new ALPriorityQueue<>();
-    ArrayList<Integer> test = new ArrayList<>();
-    test.add(1);
-    test.add(0,2);
-//
-//
-//    System.out.println(test.toString()
-//    );
-//    alque.enqueue(1,5);
-//    alque.enqueue(2,2);
-//    alque.enqueue(3,1);
-//
-//    System.out.println(alque.peek().getValue());
+    ALPriorityQueue<Integer,Integer> alque = new ALPriorityQueue<>();
 
 
-      BHPPriorityQueue<Integer,Integer> bhpque = new BHPPriorityQueue<>();
-      BHPPriorityQueue<Integer,Integer> bhpque2 = new BHPPriorityQueue<>();
+    alque.enqueue(1,5);
+    alque.enqueue(2,2);
+    alque.enqueue(3,1);
 
-      bhpque.enqueue(1,1);
-      bhpque.enqueue(2,2);
-      bhpque.enqueue(4,4);
-      bhpque.enqueue(3,3);
-      bhpque.enqueue(2,2);
-      bhpque.enqueue(3,3);
-      bhpque.dequeueMin();
-      bhpque2.enqueue(3,3);
-
-
-        for (Entry<Integer,Integer> i: bhpque.getQueue())
-        {
-            System.out.println(i.getKey());
-        }
+    System.out.println(alque.peek().getValue());
 
   }
 }
